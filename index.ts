4006f0e6a379a30bccff554b34fd93ac6995b0ae
Alex Chaphiv <alex.chaphiv@whitehawk.com>
2022-05-26 03:41:04 -0400

    feat: add file which shows the broken behavior

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { ref } from "@vue/composition-api";
+
+// Put cursor after `computed` and attempt to auto-import from `vue-demi`
+// The presence of the `ref` import somehow breaks the `computed` import.
+
+computed
